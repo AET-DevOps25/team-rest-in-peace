caa5e3caedcc65a0c2eb9a6e16df36dfccfd2060
marcbeil <marcbeil2001@gmail.com>
2025-05-21 20:31:46 +0200

    add db model and repos

---
+++ server/data-fetching-service/src/main/java/com/example/data_fetching_service/model/Person.java
@@ -0,0 +1,76 @@
+package com.example.data_fetching_service.model;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "person")
+public class Person {
+
+    @Id
+    private Integer id;
+
+    @Column(name = "speaker_id")
+    private Integer speakerId;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "party", nullable = false)
+    private String party;
+
+    @OneToMany(mappedBy = "speaker")
+    private List<Speech> speeches;
+
+    // Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getSpeakerId() {
+        return speakerId;
+    }
+
+    public void setSpeakerId(Integer speakerId) {
+        this.speakerId = speakerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getParty() {
+        return party;
+    }
+
+    public void setParty(String party) {
+        this.party = party;
+    }
+
+    public List<Speech> getSpeeches() {
+        return speeches;
+    }
+
+    public void setSpeeches(List<Speech> speeches) {
+        this.speeches = speeches;
+    }
+}
