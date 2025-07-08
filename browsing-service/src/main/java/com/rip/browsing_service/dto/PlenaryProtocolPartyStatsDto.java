970fedb25ab2f194ffa7341e114e97fa88faaedf
Ramona_Eckert <ge36moc@mytum.de>
2025-07-08 15:37:17 +0200

    Create a endpoint for PlenaryProtocols with stats

---
+++ browsing-service/src/main/java/com/rip/browsing_service/dto/PlenaryProtocolPartyStatsDto.java
@@ -0,0 +1,7 @@
+package com.rip.browsing_service.dto;
+
+public record PlenaryProtocolPartyStatsDto(
+    String party,
+    int words,
+    int percentage
+) {}
--- browsing-service/src/main/java/com/rip/browsing_service/repository/SpeechRepository.java
+++ browsing-service/src/main/java/com/rip/browsing_service/repository/SpeechRepository.java
@@ -14,4 +14,23 @@ public interface SpeechRepository extends JpaRepository<Speech, Integer> {
 
     @Query("SELECT s.textPlain FROM Speech s")
     List<String> findAllTextPlain();
+
+    // Fetch textPlain for specific agendaItemIds
+    @Query("SELECT s.textPlain FROM Speech s WHERE s.agendaItem.id IN :agendaItemIds")
+    List<String> findAllTextPlainByAgendaItemIds(List<Integer> agendaItemIds);
+
+    @Query("SELECT COUNT(DISTINCT s.person.id) FROM Speech s WHERE s.agendaItem.id IN :agendaItemIds")
+    long countDistinctPersonIdsByAgendaItemIds(List<Integer> agendaItemIds);
+
+    @Query("""
+        SELECT
+        	p.party,
+        	s.textPlain
+        FROM
+        	Speech s,
+        	Person p
+        WHERE s.agendaItem.id IN :agendaItemIds
+        	AND s.person.id = p.id
+        """)
+    List<Object[]> findPartyToSpeechTextByAgendaItemIds(List<Integer> agendaItemIds);
 }
