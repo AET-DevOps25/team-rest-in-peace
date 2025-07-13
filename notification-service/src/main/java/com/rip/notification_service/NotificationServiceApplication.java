64e998911b8e9e02852e866c77ccfeed47d828e8
marcbeil <marcbeil2001@gmail.com>
2025-07-13 15:56:30 +0200

    implement notification functionality

--- notification-service/src/main/java/com/rip/notification_service/NotificationServiceApplication.java
+++ notification-service/src/main/java/com/rip/notification_service/NotificationServiceApplication.java
@@ -1,13 +1,22 @@
 package com.rip.notification_service;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
 
 @SpringBootApplication
 public class NotificationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NotificationServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        File dotenvFile = new File(".env");
+        if (dotenvFile.exists()) {
+            Dotenv dotenv = Dotenv.configure().load();
+            dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+        }
+
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
 
 }
