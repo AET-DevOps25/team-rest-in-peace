9598bb2b96adf5cc0e49e0d8857df24179bd718f
marcbeil <marcbeil2001@gmail.com>
2025-07-14 17:01:01 +0200

    implement notifications for type: party, person

--- notification-service/src/main/java/com/rip/notification_service/repository/PlenaryProtocolRepository.java
+++ notification-service/src/main/java/com/rip/notification_service/repository/PlenaryProtocolRepository.java
@@ -2,6 +2,7 @@ package com.rip.notification_service.repository;
 
 import com.rip.notification_service.model.PlenaryProtocol;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
