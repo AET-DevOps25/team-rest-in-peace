978e343e84c7543f812c12ce2cb5387333709f4b
marcbeil <marcbeil2001@gmail.com>
2025-07-14 23:46:15 +0200

    fix default env var

--- client/src/global.ts
+++ client/src/global.ts
@@ -1,5 +1,5 @@
 export const BROWSING_BASE_URL =
-  import.meta.env.VITE_BROWSING_BASE_URL || "http://localhost:8081";
+  import.meta.env.VITE_BROWSING_BASE_URL || "/browse";
 
 export function getPartyColor(party: string) {
   const colors = {
