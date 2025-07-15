680657b9a621249492668480c93814fc3bc71128
marcbeil <marcbeil2001@gmail.com>
2025-07-15 16:56:14 +0200

    fix url bugs

--- notification-service/src/main/java/com/rip/notification_service/EmailService.java
+++ notification-service/src/main/java/com/rip/notification_service/EmailService.java
@@ -27,7 +27,7 @@ public class EmailService {
               <body>
                 <p>Dear Subscriber,</p>
                 <p>There have been %d new plenary protocol(s) published.</p>
-                <p>You can view them <a href="%s/plenary-protocols">here</a>.</p>
+                <p>You can view them <a href="%s">here</a>.</p>
                 <p>If you no longer wish to receive these notifications, you can <a href="%s">unsubscribe here</a>.</p>
                 <hr/>
                 <p>Best regards,<br/>Policy Watch Team</p>
@@ -80,7 +80,7 @@ public class EmailService {
                     String partyUrl = UriComponentsBuilder
                             .fromUriString(clientBaseUrl)
                             .path("/party")
-                            .queryParam("partei", party)
+                            .queryParam("party", party)
                             .build()
                             .encode()
                             .toUriString();
